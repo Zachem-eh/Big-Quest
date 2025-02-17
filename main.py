68fa35834be0e9096d586ba77c40e2b282a48dc5
Zachem-eh <vladsasha7002@gmail.com>
2025-02-17 19:00:20 +0300

    number 2

--- main.py
+++ main.py
@@ -8,29 +8,41 @@ pygame.init()
 size = width, height = 600, 450
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Москва')
+z_const = 10
 
 
-url = 'https://static-maps.yandex.ru/v1'
-params = {
-    'apikey': '4e60121e-3e68-41f0-bd84-eced30775d1c',
-    'll': '73.088504,49.807760',
-    'z': '10',
-}
+def make_request(new_z):
+    url = 'https://static-maps.yandex.ru/v1'
+    params = {
+        'apikey': '4e60121e-3e68-41f0-bd84-eced30775d1c',
+        'll': '73.088504,49.807760',
+        'z': str(new_z),
+    }
 
-response = requests.get(url, params)
-if not response:
-    print(f'{response.status_code} {response.reason}')
-    sys.exit(1)
+    response = requests.get(url, params)
+    if not response:
+        print(f'{response.status_code} {response.reason}')
+        sys.exit(1)
 
-with open('map.png', mode='wb') as map_file:
-    map_file.write(response.content)
+    with open('map.png', mode='wb') as map_file:
+        map_file.write(response.content)
 
+    map_image = pygame.image.load('map.png')
+    return map_image
+
+
+map_im = make_request(z_const)
 running = True
-map_im = pygame.image.load('map.png')
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                z_const = z_const + 1 if z_const != 21 else 21
+            elif event.key == pygame.K_PAGEDOWN:
+                z_const = z_const - 1 if z_const != 0 else 0
+            map_im = make_request(z_const)
     screen.blit(map_im, (0, 0))
     pygame.display.flip()
 
